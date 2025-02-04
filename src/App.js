3e5b46fedf0512643d4613dd4fdb876364f7c790
Shreya Shrivastava <147529420+ShreyaS-2321@users.noreply.github.com>
2025-02-05 03:08:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";
-import { useEffect } from "react";
- 
+import React, { useState, useEffect } from "react";
+
 function App() {
   const [city, setCity] = useState("Delhi");
   const [weatherData, setWeatherData] = useState(null);
-  const [error, setError] = useState(null);
- 
+
   const currentDate = new Date();
- 
+
   const months = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ];
- 
+
   const month = months[currentDate.getMonth()];
   const day = currentDate.getDate();
   const year = currentDate.getFullYear();
- 
+
   const formattedDate = `${month} ${day}, ${year}`;
- 
+
   const API_KEY = "bcda10ba323e88e96cb486015a104d1d"; // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
- 
+
   const fetchWeatherData = async () => {
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       );
       const data = await response.json();
-      console.log(data)
-      // if (response.ok) {
+      console.log(data);
       setWeatherData(data);
-      // setError(null);
-      // } else {
-      //   setError(data.message);
-      //   setWeatherData(null);
-      // }
     } catch (error) {
       console.error("Error fetching weather data:", error);
-      // setError("Error fetching weather data. Please try again later.");
-      // setWeatherData(null);
     }
   };
- 
-  useEffect(()=>{
- 
-     
-   
- 
-  fetchWeatherData();
- 
-  },[])
- 
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, [city]); // Add 'city' to the dependency array to refetch when it changes
+
   const handleInputChange = (event) => {
     setCity(event.target.value);
   };
- 
+
   const handleSubmit = (event) => {
     event.preventDefault();
     fetchWeatherData();
   };
- 
+
   const getWeatherImg = (main) => {
     switch (main) {
       case "Clear":
@@ -73,26 +57,22 @@ function App() {
       case "Haze":
         return "/images/sun.png";
       case "Clouds":
-          return "/images/clouds.png";
+        return "/images/clouds.png";
       case "Mist":
         return "/images/mist.png"
       default:
         return null;
     }
   };
- 
- 
+
   return (
     <div className="App">
-       
- 
       <div className="container">
         {weatherData && (
           <>
             <h1 className="container_date">{formattedDate}</h1>
             <div className="weather_data">
               <h2 className="container_city">{weatherData.name}</h2>
-              {/* <img className="container_img" src="/thunder.png" width="180px" alt="sss"/> */}
               <img className="container_img" src={getWeatherImg(weatherData.weather[0].main)} width="180px" alt="Weather Icon" />
               <h2 className="container_degree">{weatherData.main.temp}</h2>
               <h2 className="country_per">{weatherData.weather[0].main}<span className="degree_icon"></span></h2>
@@ -110,8 +90,6 @@ function App() {
             </div>
           </>
         )}
- 
- 
       </div>
     </div>
   );
